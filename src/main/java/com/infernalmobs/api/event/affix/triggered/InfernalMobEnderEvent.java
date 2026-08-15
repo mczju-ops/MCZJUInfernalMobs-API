59984aab52fb33f567276426df6f96de506cc31f
tozero <74749129+tozero0@users.noreply.github.com>
2026-08-15 23:53:37 +0800

    增强 ender 事件的传送目的地参数

--- src/main/java/com/infernalmobs/api/event/affix/triggered/InfernalMobEnderEvent.java
+++ src/main/java/com/infernalmobs/api/event/affix/triggered/InfernalMobEnderEvent.java
@@ -3,17 +3,38 @@ package com.infernalmobs.api.event.affix.triggered;
 import com.infernalmobs.api.InfernalMobHandle;
 import com.infernalmobs.api.event.affix.InfernalAffixTriggeredEvent;
 import com.infernalmobs.skill.SkillType;
+import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-/** 末影（ender）词条真正触发事件。 */
+import java.util.Objects;
+
+/**
+ * 末影（ender）词条真正触发事件。
+ * 外部插件可修改怪物本次传送的最终目的地。
+ */
 public class InfernalMobEnderEvent extends InfernalAffixTriggeredEvent {
 
     private static final HandlerList HANDLERS = new HandlerList();
 
-    public InfernalMobEnderEvent(LivingEntity mob, LivingEntity target, InfernalMobHandle handle, int level) {
+    private Location destination;
+
+    public InfernalMobEnderEvent(LivingEntity mob, LivingEntity target, InfernalMobHandle handle, int level,
+                                 @NotNull Location destination) {
         super("ender", SkillType.DUAL, mob, target, handle, level);
+        setDestination(destination);
+    }
+
+    /** 怪物本次传送的最终目的地。 */
+    @NotNull
+    public Location getDestination() {
+        return destination;
+    }
+
+    /** 修改怪物本次传送的最终目的地。 */
+    public void setDestination(@NotNull Location destination) {
+        this.destination = Objects.requireNonNull(destination, "destination");
     }
 
     @Override
